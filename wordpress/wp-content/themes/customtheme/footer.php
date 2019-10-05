194e8c758a3935d214badcb992a9c5ddad83fba3
Quý <toxuanquy@users.noreply.github.com>
2019-10-05 08:19:13 +0700

    Initial commit

---
+++ wordpress/wp-content/themes/customtheme/footer.php
@@ -0,0 +1,13 @@
+<footer id="footer" >
+
+    <!-- Copyright -->
+    <div class="copyright">
+        © <?php echo date('Y'); ?> <?php bloginfo( 'sitename' ); ?>. <?php _e('Tecnology cational', 'quyto'); ?>
+    </div>
+</footer>
+</div> <!-- closes <div class=container"> -->
+
+<?php wp_footer() ?>
+</body>
+
+</html>
